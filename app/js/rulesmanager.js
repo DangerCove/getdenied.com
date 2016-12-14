50c3dc38585438d016de24ba3e3f877e90b6b522
Boy van Amstel <boy@boyvanamstel.nl>
2016-12-14 15:22:30 +0100

    Start vanilla js rules-manager

--- app/js/rulesmanager.js
+++ app/js/rulesmanager.js
@@ -3,8 +3,28 @@
 class RulesManager {
   constructor(el) {
     this.el = el;
-    console.log(el);
+
+    this._bindFileUpload();
+  }
+
+  _bindFileUpload() {
+    var fileContainer = this.el.getElementsByClassName('rules-manager__button-container--load')[0];
+    var fileLink = fileContainer.getElementsByClassName('rules-manager__load')[0];
+    var fileButton = fileContainer.getElementsByClassName('rules-manager__file')[0];
+
+    fileLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      fileButton.click();
+    });
   }
 }
 
-//var rulesManager = new RulesManager('blah');
+// Activate
+(function() {
+  var elements = document.getElementsByClassName('rules-manager');
+  console.log(elements);
+  for (var i = 0; i < elements.length; i++) { 
+    var el = elements[i];
+    var rulesManager = new RulesManager(el);
+  }
+})();
